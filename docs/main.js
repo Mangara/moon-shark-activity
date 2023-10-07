7e5e239105b3407e1bc4ad9c02493c42ce15d362
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2023-10-07 13:23:42 -0400

    Update displacement scale

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
+const RADIUS = 2.0
+
+function displacementScale() {
+	// unsigned 16-bit TIFFs in half-meters, relative to a radius of 1727400 meters.
+	const trueScale = 2 * RADIUS / 1727400;
+	return 1000 * trueScale; // Emphasize differences to be very slightly noticeable
+}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -13,25 +21,24 @@ const sunlight = new THREE.DirectionalLight( 0xffffff, 0.9 );
 sunlight.position.set( -1, 0, 0.3 )
 scene.add( sunlight );
 
-const geometry = new THREE.SphereGeometry( 2, 1024, 512 );
+const geometry = new THREE.SphereGeometry( RADIUS, 1024, 512 );
 
 const surfaceTexture = new THREE.TextureLoader().load('assets/lroc_color_poles_2k.png');
 const displacement = new THREE.TextureLoader().load('assets/ldem_4_uint_mod.png');
 const material = new THREE.MeshStandardMaterial( { 
 	map: surfaceTexture,
 	displacementMap: displacement,
-	displacementScale: 0.1,
+	displacementScale: displacementScale(),
 } );
 const moon = new THREE.Mesh( geometry, material );
 scene.add( moon );
 
-camera.position.z = 5;
+camera.position.z = 4;
 
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight - 100 );
 document.body.appendChild( renderer.domElement );
-
 
 function animate() {
 	requestAnimationFrame( animate );
