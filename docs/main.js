74a5a88816caf282f2217216e51bb4592ad902d7
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2023-10-07 11:00:26 -0400

    Render sphere

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,14 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.SphereGeometry( 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0xa4bdd5 } );
+const moon = new THREE.Mesh( geometry, material );
+scene.add( moon );
 
 camera.position.z = 5;
 
@@ -19,8 +20,14 @@ document.body.appendChild( renderer.domElement );
 
 function animate() {
 	requestAnimationFrame( animate );
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	moon.rotation.x += 0.01;
+	moon.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
-animate();
+
+if ( WebGL.isWebGLAvailable() ) {
+	animate();
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	document.body.appendChild( warning );
+}
