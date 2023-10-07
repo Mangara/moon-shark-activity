ed73dbba2721b8239ad3c343e5fa969bca27e81a
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2023-10-07 13:08:00 -0400

    More realistic moon

--- docs/main.js
+++ docs/main.js
@@ -4,9 +4,24 @@ import WebGL from 'three/addons/capabilities/WebGL.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+// Ambient light to make sure everything is slightly visible
+const global_light = new THREE.AmbientLight( 0xffffff, 0.05 ); // soft white light
+scene.add( global_light );
 
-const geometry = new THREE.SphereGeometry( 2 );
-const material = new THREE.MeshBasicMaterial( { color: 0xa4bdd5 } );
+// White directional light shining from the left
+const sunlight = new THREE.DirectionalLight( 0xffffff, 0.9 );
+sunlight.position.set( -1, 0, 0.3 )
+scene.add( sunlight );
+
+const geometry = new THREE.SphereGeometry( 2, 1024, 512 );
+
+const surfaceTexture = new THREE.TextureLoader().load('assets/lroc_color_poles_2k.png');
+const displacement = new THREE.TextureLoader().load('assets/ldem_4_uint_mod.png');
+const material = new THREE.MeshStandardMaterial( { 
+	map: surfaceTexture,
+	displacementMap: displacement,
+	displacementScale: 0.1,
+} );
 const moon = new THREE.Mesh( geometry, material );
 scene.add( moon );
 
@@ -20,8 +35,7 @@ document.body.appendChild( renderer.domElement );
 
 function animate() {
 	requestAnimationFrame( animate );
-	moon.rotation.x += 0.01;
-	moon.rotation.y += 0.01;
+	moon.rotation.y += 0.003;
 	renderer.render( scene, camera );
 }
 
