58e69546b3616fb0d3119d9bcef319e157c4b20c
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2023-10-07 22:15:53 -0400

    Add mouse controls

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,9 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000000 );
-camera.position.z = 1000;
 
 // Ambient light to make sure everything is slightly visible
 const global_light = new THREE.AmbientLight( 0xffffff, 0.1 ); // soft white light
@@ -34,10 +33,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight - 100 );
 document.body.appendChild( renderer.domElement );
 
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100000 );
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enablePan = false;
+controls.minDistance = 501;
+controls.maxDistance = 100000;
+
+camera.position.z = 1000;
+controls.update();
+
+var rotationRate = 0.003;
+
 function animate() {
 	requestAnimationFrame( animate );
-	// moon.rotation.y += 0.003;
-	if (moon != undefined) { moon.rotation.y += 0.003; }
+	if (moon != undefined) { moon.rotation.y += rotationRate; }
+	controls.update();
 	renderer.render( scene, camera );
 }
 
@@ -47,3 +57,9 @@ if ( WebGL.isWebGLAvailable() ) {
 	const warning = WebGL.getWebGLErrorMessage();
 	document.body.appendChild( warning );
 }
+
+const ROTATION_ZOOM_FACTOR = 1.04;
+window.addEventListener("wheel", event => {
+    const zoomFactor = (Math.sign(event.deltaY) > 0 ? ROTATION_ZOOM_FACTOR : 1.0 / ROTATION_ZOOM_FACTOR);
+    rotationRate *= zoomFactor;
+});
