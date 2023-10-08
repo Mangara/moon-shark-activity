e5b3836d9cb722f8ef3372cfb6a755e3b7617a2d
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2023-10-07 21:38:27 -0400

    Use 3D model instead of textured sphere

--- docs/main.js
+++ docs/main.js
@@ -1,40 +1,34 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
-
-const RADIUS = 2.0
-
-function displacementScale() {
-	// unsigned 16-bit TIFFs in half-meters, relative to a radius of 1727400 meters.
-	const trueScale = 2 * RADIUS / 1727400;
-	return 1000 * trueScale; // Emphasize differences to be very slightly noticeable
-}
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000000 );
+camera.position.z = 1000;
 
 // Ambient light to make sure everything is slightly visible
-const global_light = new THREE.AmbientLight( 0xffffff, 0.05 ); // soft white light
+const global_light = new THREE.AmbientLight( 0xffffff, 0.1 ); // soft white light
 scene.add( global_light );
 
 // White directional light shining from the left
-const sunlight = new THREE.DirectionalLight( 0xffffff, 0.9 );
-sunlight.position.set( -1, 0, 0.3 )
+const sunlight = new THREE.DirectionalLight( 0xffffff, 10 );
+sunlight.position.set( -10000, 0, 3000 )
 scene.add( sunlight );
 
-const geometry = new THREE.SphereGeometry( RADIUS, 1024, 512 );
-
-const surfaceTexture = new THREE.TextureLoader().load('assets/lroc_color_poles_2k.png');
-const displacement = new THREE.TextureLoader().load('assets/ldem_4_uint_mod.png');
-const material = new THREE.MeshStandardMaterial( { 
-	map: surfaceTexture,
-	displacementMap: displacement,
-	displacementScale: displacementScale(),
-} );
-const moon = new THREE.Mesh( geometry, material );
-scene.add( moon );
-
-camera.position.z = 4;
-
+const loader = new GLTFLoader();
+var moon = undefined;
+loader.load( 
+	'assets/Moon_1_3474.glb', 
+	function ( gltf ) {
+		moon = gltf.scene;
+		scene.add( moon );
+	},
+	undefined, 
+	function ( error ) {
+		console.error( "Error loading 3D model" );
+		console.error( error );
+	}
+);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight - 100 );
@@ -42,7 +36,8 @@ document.body.appendChild( renderer.domElement );
 
 function animate() {
 	requestAnimationFrame( animate );
-	moon.rotation.y += 0.003;
+	// moon.rotation.y += 0.003;
+	if (moon != undefined) { moon.rotation.y += 0.003; }
 	renderer.render( scene, camera );
 }
 
